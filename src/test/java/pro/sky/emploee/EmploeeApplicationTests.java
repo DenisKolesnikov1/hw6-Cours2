7b712f1c4da176927189872dfc9afa88fdd77c5f
Денис Колесников <73371887@mail.ru>
2024-04-07 17:51:47 +0300

    Homework

---
+++ src/test/java/pro/sky/emploee/EmploeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.emploee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
